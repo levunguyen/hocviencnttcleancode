771e0efa2f4a01db0d6e9d28c75efad6a99fb9ee
nguyenle <nguyen.levu@smartdev.vn>
2016-10-27 11:43:01 +0700

    onelevel of abstraction

--- function/src/main/java/function/oneleveofabstractionperfunction/Example.java
+++ function/src/main/java/function/oneleveofabstractionperfunction/Example.java
@@ -1,5 +1,15 @@
 package function.oneleveofabstractionperfunction;
 
-public class Example {
+import function.problem.PathParser;
+import function.problem.WikiPagePath;
 
+public class Example {
+	
+	private Object suiteTeardown;
+
+	// It is multiple abstraction 
+	WikiPagePath pagePath = suiteTeardown.getPageCrawler().getFullPath (suiteTeardown);
+	
+	//It is good . Only one level of abstraction
+	String pagePathName = PathParser.render(pagePath);
 }
