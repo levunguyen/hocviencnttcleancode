a6c4875a8d45ca8fb4c3d8018dc9136b3378055b
nguyenle <nguyen.levu@smartdev.vn>
2016-10-27 11:15:47 +0700

    ok

--- meaningfulname/src/main/java/meaningfulname/pronounceable/Example1.java
+++ meaningfulname/src/main/java/meaningfulname/pronounceable/Example1.java
@@ -1,5 +1,17 @@
 package meaningfulname.pronounceable;
 
+import java.util.Date;
+
 public class Example1 {
 
+	//A company I know has genymdhms (generation date, year, month, day, hour, minute, and second) 
+	//bad
+	private Date genymdhms;
+	private Date modymdhms;
+	private final String pszqint = "102";
+	
+	//good 
+	private Date generationTimestamp; 
+	private Date modificationTimestamp;
+	private final String recordId = "102";
 }
