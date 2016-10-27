50e5c4e82fded3505aadea6027d0e28cf9dbe9ac
nguyenle <nguyen.levu@smartdev.vn>
2016-10-27 10:56:28 +0700

    meaning

--- meaningfulname/src/main/java/meaningfulname/revealingname/Example2.java
+++ meaningfulname/src/main/java/meaningfulname/revealingname/Example2.java
@@ -1,6 +1,25 @@
 package meaningfulname.revealingname;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Example2 {
 
+	//problem
 	
+	public List<int[]> getThem() {
+		List<int[]> list1 = new ArrayList<int[]>();
+		for (int[] x : theList)
+			if (x[0] == 4)
+				list1.add(x);
+		return list1;
+	}
+	
+//	question
+//
+//1. What kinds of things are in theList?
+//2. What is the significance of the zeroth subscript of an item in theList?
+//3. What is the significance of the value 4?
+//4. How would I use the list being returned?
+
 }
