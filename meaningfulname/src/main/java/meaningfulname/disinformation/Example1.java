c324ff80c303e02e48ed3db3256cc463271b6ae9
nguyenle <nguyen.levu@smartdev.vn>
2016-10-27 11:04:40 +0700

    disinformation

--- meaningfulname/src/main/java/meaningfulname/disinformation/Example1.java
+++ meaningfulname/src/main/java/meaningfulname/disinformation/Example1.java
@@ -1,5 +1,15 @@
 package meaningfulname.disinformation;
 
+import java.util.List;
+
 public class Example1 {
 
+	//it is very bad because it references to unix system
+	private String hp, aix, sco;
+	
+	//it is bad.The word list means something specific to programmers
+	private List<String> accountList;
+	
+	//it is better
+	private List<String> accountGroup,accounts,groupofAccount ;
 }
