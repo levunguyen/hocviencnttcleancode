24d0109814cae2825b5a9e43292f79f0e2203690
nguyenle <nguyenle@10.12.2.91>
2016-11-04 14:49:34 +0700

    go

--- function/src/main/java/function/flagagurment/Example.java
+++ function/src/main/java/function/flagagurment/Example.java
@@ -2,4 +2,27 @@ package function.flagagurment;
 
 public class Example {
 
+	//not good to pass boolean as parameter. If we pass true it will do
+	// 2 things. First it checks if the flag is true or not then it decide to do 
+	
+	public void renderTest(boolean flag) {
+		
+		if (true) {
+			do someting
+		} else {
+			do something
+		}
+		
+	}
+	
+	
+	///we should have 2 seperate methods
+	
+	public void renderForSuite() {
+		
+	}
+	
+	public void renderForSingleTest() {
+		
+	}
 }
