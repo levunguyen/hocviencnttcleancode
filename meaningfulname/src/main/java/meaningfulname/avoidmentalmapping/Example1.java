b232cdeba173d85ffe92977deb17863c565d622c
nguyenle <nguyenle@10.12.2.91>
2016-11-04 14:40:04 +0700

    mental mapping

--- meaningfulname/src/main/java/meaningfulname/avoidmentalmapping/Example1.java
+++ meaningfulname/src/main/java/meaningfulname/avoidmentalmapping/Example1.java
@@ -2,4 +2,10 @@ package meaningfulname.avoidmentalmapping;
 
 public class Example1 {
 
+	
+	for (int i=0,i<size,i++) {
+		if (i == k) {
+			
+		}
+	}
 }
--- meaningfulname/src/main/java/meaningfulname/classname/Customer.java
+++ meaningfulname/src/main/java/meaningfulname/classname/Customer.java
@@ -1,5 +1,5 @@
 package meaningfulname.classname;
 
 public class Customer {
-
+ good
 }
