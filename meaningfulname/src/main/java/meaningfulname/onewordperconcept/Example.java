2057da8211f33ffa6530ca16ebb36ee9dc0b86b4
nguyenle <nguyenle@10.12.2.91>
2016-11-04 14:24:46 +0700

    comit

--- meaningfulname/src/main/java/meaningfulname/onewordperconcept/Example.java
+++ meaningfulname/src/main/java/meaningfulname/onewordperconcept/Example.java
@@ -1,5 +1,21 @@
 package meaningfulname.onewordperconcept;
 
+import java.util.List;
+
 public class Example {
 
+	//choose only one concept
+	
+	public List<Employee> getEmployees() {
+		return null;
+	}
+	
+	public List<Employee> retrieveEmployees() {
+		return null;
+	}
+	
+	public List<Employee> getEmployees() {
+		return null
+	}
+	
 }
