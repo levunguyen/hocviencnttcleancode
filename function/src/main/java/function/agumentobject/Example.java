978dcba4c3f6dd223b11c847688bf489f611d6d4
framgia <framgia@framgia-Inspiron-3459>
2016-11-17 15:14:48 +0700

    agument object

--- function/src/main/java/function/agumentobject/Example.java
+++ function/src/main/java/function/agumentobject/Example.java
@@ -1,5 +1,17 @@
 package function.agumentobject;
 
 public class Example {
+	
+	//When a function seems to need more than two or three arguments, it is likely that some of
+	//those arguments ought to be wrapped into a class of their own
+	
+	//b
+	Circle makeCircle(double x, double y, double radius);
+	
+	//good
+	Circle makeCircle(Point center, double radius);
+	
+	
+	//When we create a paramater we should think about the paramter 1,2 is enough
 
 }
